239899bdd687504dd1adaae3a756becd5775bac2
T-An <lyhotuanan2004@gmail.com>
2024-07-31 00:11:10 +0700

    giao_dien base

---
+++ giaodien/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace giaodien
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.btnProjects = new System.Windows.Forms.Button();
+            this.btnDashBoard = new System.Windows.Forms.Button();
+            this.lbl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAllFiles = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.viewAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yourFilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sharedFilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.btnAllFiles);
+            this.splitContainer1.Panel1.Controls.Add(this.btnProjects);
+            this.splitContainer1.Panel1.Controls.Add(this.btnDashBoard);
+            this.splitContainer1.Panel1.Controls.Add(this.lbl);
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.panel1);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint);
+            this.splitContainer1.Size = new System.Drawing.Size(1332, 892);
+            this.splitContainer1.SplitterDistance = 444;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // btnProjects
+            // 
+            this.btnProjects.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProjects.ForeColor = System.Drawing.Color.Black;
+            this.btnProjects.Location = new System.Drawing.Point(104, 274);
+            this.btnProjects.Name = "btnProjects";
+            this.btnProjects.Size = new System.Drawing.Size(222, 64);
+            this.btnProjects.TabIndex = 2;
+            this.btnProjects.Text = "Projects";
+            this.btnProjects.UseVisualStyleBackColor = true;
+            // 
+            // btnDashBoard
+            // 
+            this.btnDashBoard.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDashBoard.ForeColor = System.Drawing.Color.Black;
+            this.btnDashBoard.Location = new System.Drawing.Point(104, 136);
+            this.btnDashBoard.Name = "btnDashBoard";
+            this.btnDashBoard.Size = new System.Drawing.Size(222, 64);
+            this.btnDashBoard.TabIndex = 1;
+            this.btnDashBoard.Text = "DashBoard";
+            this.btnDashBoard.UseVisualStyleBackColor = true;
+            // 
+            // lbl
+            // 
+            this.lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.lbl.Location = new System.Drawing.Point(125, 31);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(201, 49);
+            this.lbl.TabIndex = 0;
+            this.lbl.Text = "Space Box";
+            this.lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(45, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(201, 49);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Iconly";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnAllFiles
+            // 
+            this.btnAllFiles.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAllFiles.ForeColor = System.Drawing.Color.Black;
+            this.btnAllFiles.Location = new System.Drawing.Point(111, 414);
+            this.btnAllFiles.Name = "btnAllFiles";
+            this.btnAllFiles.Size = new System.Drawing.Size(222, 64);
+            this.btnAllFiles.TabIndex = 4;
+            this.btnAllFiles.Text = "All Files";
+            this.btnAllFiles.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.menuStrip1);
+            this.panel1.Location = new System.Drawing.Point(15, 245);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(857, 635);
+            this.panel1.TabIndex = 5;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewAllToolStripMenuItem,
+            this.yourFilesToolStripMenuItem,
+            this.sharedFilesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(857, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // viewAllToolStripMenuItem
+            // 
+            this.viewAllToolStripMenuItem.Name = "viewAllToolStripMenuItem";
+            this.viewAllToolStripMenuItem.Size = new System.Drawing.Size(77, 24);
+            this.viewAllToolStripMenuItem.Text = "View All";
+            // 
+            // yourFilesToolStripMenuItem
+            // 
+            this.yourFilesToolStripMenuItem.Name = "yourFilesToolStripMenuItem";
+            this.yourFilesToolStripMenuItem.Size = new System.Drawing.Size(83, 24);
+            this.yourFilesToolStripMenuItem.Text = "Your files";
+            // 
+            // sharedFilesToolStripMenuItem
+            // 
+            this.sharedFilesToolStripMenuItem.Name = "sharedFilesToolStripMenuItem";
+            this.sharedFilesToolStripMenuItem.Size = new System.Drawing.Size(104, 24);
+            this.sharedFilesToolStripMenuItem.Text = "Shared files ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1332, 892);
+            this.Controls.Add(this.splitContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button btnProjects;
+        private System.Windows.Forms.Button btnDashBoard;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAllFiles;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem viewAllToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yourFilesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sharedFilesToolStripMenuItem;
+    }
+}
+
